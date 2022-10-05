8d30b7d6a38a873bb0bf32ae0cb4db1f65f1134c
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2022-10-05 11:55:48 -0400

    added other todos

--- components/displayTodoistShoppingList.tsx
+++ components/displayTodoistShoppingList.tsx
@@ -1,25 +1,55 @@
 import { useQuery } from "@tanstack/react-query";
+import { ProjectList, taskList } from "../pages/api/todoistShoppingList";
 
 export default function DisplayTodoistShoppingList() {
+  const twoMinutes = 1000 * 60 * 2;
   const { data } = useQuery(
-    ["todoist", "shopping"],
+    ["todoist"],
     () => fetch("api/todoistShoppingList").then((res) => res.json()),
     {
-      refetchInterval: 60000,
+      refetchInterval: twoMinutes,
     }
   );
 
   if (!data) return <div>Loading...</div>;
-  console.log(data);
+  const shoppingListName = Object.keys(data).filter((key) =>
+    key.includes("Shopping")
+  )[0];
+  const shoppingList = data[shoppingListName];
+  const otherProjects = Object.keys(data).filter(
+    (key) => !key.includes("Shopping")
+  );
+  const otherTasks: any = [];
+
+  otherProjects.forEach((project) => {
+    otherTasks.push(...data[project]);
+  });
+
+  // add project name to each task
+  otherTasks.forEach((task: any) => {
+    task.project = Object.keys(data).filter((key) =>
+      data[key].includes(task)
+    )[0];
+  });
+  console.log(otherTasks);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="text-4xl font-bold">Todoist Shopping List</div>
       <div className="text-2xl font-bold">
-        {data?.map((item: any) => {
+        {shoppingList?.map((item: any) => {
           const date = new Date(item.createdAt);
           return (
             <div key={item.id}>
               {item.content} - {date.toLocaleDateString()}
+            </div>
+          );
+        })}
+        {otherTasks?.map((item: any) => {
+          const date = new Date(item.createdAt);
+          return (
+            <div key={item.id}>
+              {item.content} - {date.toLocaleDateString()} - {item.project}
             </div>
           );
         })}
--- pages/api/todoistShoppingList.ts
+++ pages/api/todoistShoppingList.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { TodoistApi } from "@doist/todoist-api-typescript";
+import { Task } from "@doist/todoist-api-typescript/dist/types/entities";
+
+export type taskList = Task[];
+export interface ProjectList {
+  [key: string]: taskList;
+}
 
 export default async function getShoppingList(
   req: NextApiRequest,
@@ -7,9 +13,16 @@ export default async function getShoppingList(
 ) {
   const api = new TodoistApi(process.env.TODOIST_API_KEY as string);
   const projects = await api.getProjects();
-  const shoppingList = projects.find((project) =>
-    project.name.includes("hopping")
+
+  const allTasks = await api.getTasks();
+  const allProjectsWithTasks = projects.reduce(
+    (projectsToSend: ProjectList, project) => {
+      const tasks = allTasks.filter((task) => task.projectId === project.id);
+      projectsToSend[project.name] = tasks;
+      return projectsToSend;
+    },
+    {}
   );
-  const shoppingListItems = await api.getTasks({ projectId: shoppingList?.id });
-  res.status(200).json(shoppingListItems);
+
+  res.status(200).json(allProjectsWithTasks);
 }

7a2921d9d9c2db635ccd4a8705244e2f243eaacb
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2022-10-08 10:34:29 -0400

    fix types for calendar

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,23 +4,36 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 type CalendarProps = {
   eventList: any[];
 };
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+  color: string;
+  allDay?: boolean;
+}
+
+const getFormattedEvents = (eventList: any[]): Event[] => {
+  const formattedEvents: Event[] = eventList.map((event): Event => {
+    const startTime = new Date(event.start.dateTime);
+    const endTime = new Date(event.end.dateTime);
+
+    const newEvent = {
+      title: event.summary,
+      start: startTime,
+      end: endTime,
+      color: event.eventColor,
+    };
+    return newEvent;
+  });
+  return formattedEvents;
+};
 
 export function DayCalendar({ eventList }: CalendarProps) {
   console.log(eventList);
   const formattedEventList =
-    eventList?.map((event) => {
-      const startTime = new Date(event.start.dateTime);
-      const endTime = new Date(event.end.dateTime);
-
-      return {
-        title: event.summary,
-        start: startTime,
-        end: endTime,
-        color: event.eventColor,
-      };
-    }) || [];
+    eventList && eventList.length > 0 ? getFormattedEvents(eventList) : [];
   return (
-    <div className="col-span-2">
+    <div className="col-span-1">
       <FullCalendar
         plugins={[timeGridPlugin, dayGridPlugin]}
         initialView="timeGrid"
@@ -38,18 +51,9 @@ export function DayCalendar({ eventList }: CalendarProps) {
 export function MonthCalendar({ eventList }: CalendarProps) {
   console.log(eventList);
   const formattedEventList =
-    eventList?.map((event) => {
-      const startTime = new Date(event.start.dateTime);
-      const endTime = new Date(event.end.dateTime);
-      return {
-        title: event.summary,
-        start: startTime,
-        end: endTime,
-        color: event.eventColor,
-      };
-    }) || [];
+    eventList && eventList.length > 0 ? getFormattedEvents(eventList) : [];
   return (
-    <div className="col-span-3">
+    <div className="col-span-4">
       <FullCalendar
         plugins={[timeGridPlugin, dayGridPlugin]}
         initialView="dayGridMonth"

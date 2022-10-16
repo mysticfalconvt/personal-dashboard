dcc6cc1026edc9ceff0d927e8313ce47efc9efea
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2022-10-16 18:22:05 -0400

    moved refetch image into R-Q

--- components/background.tsx
+++ components/background.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { backgroundRefetchTime, photoChangeTime } from "../constants";
 
 const fetchBackground = async () => {
@@ -18,12 +18,20 @@ const getTextTheme = (color: number[]) => {
   const green = color[1];
   const blue = color[2];
   const brightness = (red * 299 + green * 587 + blue * 114) / 1000;
-  return brightness > 150 ? "text-black" : "text-white";
+  return brightness > 140 ? "text-black" : "text-white";
 };
 
 type Props = {
   setTheme: (theme: string) => void;
 };
+
+const getCurrentBackground = async (urls: any[]) => {
+  const image = urls[Math.floor(Math.random() * urls.length)];
+  const imageAverageColor = await getColor(image);
+  const textTheme = getTextTheme(imageAverageColor);
+  return { image, textTheme };
+};
+
 // get a background image from the background api
 export default function Background({ setTheme }: Props) {
   const { data } = useQuery(["background"], fetchBackground, {
@@ -32,23 +40,26 @@ export default function Background({ setTheme }: Props) {
     staleTime: 0,
   });
 
-  const [image, setImage] = useState(data[0]);
-  const [nextImage, setNextImage] = useState(data[0]);
+  const { data: currentDisplay } = useQuery(
+    ["color", "displayImage"],
+    () => getCurrentBackground(data),
+    {
+      refetchInterval: photoChangeTime,
+      staleTime: 0,
+      enabled: data?.length > 1,
+    }
+  );
+  console.log(currentDisplay);
+  const color =
+    currentDisplay?.textTheme === "text-black" ? "text-black" : "text-white";
 
-  // switch to a new image every n seconds
-  setTimeout(async () => {
-    const color = await getColor(nextImage);
-    const textColor = getTextTheme(color);
-    // console.log("Text color: ", textColor);
-    setTheme(getTextTheme(color));
-    setImage(nextImage);
-
-    setNextImage(data[Math.floor(Math.random() * data.length)]);
-  }, photoChangeTime);
+  useEffect(() => {
+    setTheme(color);
+  }, [color, setTheme]);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full z-0">
-      <img src={image} className="w-full h-full object-cover" />
+      <img src={currentDisplay?.image} className="w-full h-full object-cover" />
     </div>
   );
 }
